6d92340c2d472308b88783d4ea6b0114d40da5ca
Kaden Cartwright <kaden@deepspacerobots.com>
2022-05-02 12:51:14 -0500

    basic bootstrapping/clap setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use clap::Parser;
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// message to echo
+    #[clap(short, long)]
+    message: String,
+
+    /// Number of times to echo
+    #[clap(short, long, default_value_t = 1)]
+    count: u8,
+}
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    for _ in 0..args.count {
+        println!("{}", args.message)
+    }
+}
+pub struct ConfigFileReader {}
+
+#[cfg(test)]
+mod tests {
+    /// it should create test config files if not exist
+    #[test]
+    fn it_creates_config() {
+        assert_eq!(2 + 2, 5);
+    }
+    ///it should not overwrite the file configif it already exists
+    #[test]
+    fn it_doesnt_overwrite_config() {
+        assert_eq!(2 + 2, 5);
+    }
 }
